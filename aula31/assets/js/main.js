448571cde0111f7a5010852929ace408b9c61c2f
Franccesco Antonio <franccesco_@hotmail.com>
2020-09-25 16:33:56 -0300

    aula31

---
+++ aula31/assets/js/main.js
@@ -0,0 +1,35 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco {... bloco}
+// Var só tem escopo de função
+
+// let nome = 'Franccesco'; // criando
+// var nome2 = 'Franccesco'; //criando
+
+// if (verdadeira) {
+// 	let nome = 'Antonio' // Criando
+// 	var nome2 = 'Bordon' // redeclarando
+
+// 	if (verdadeira) {
+// 		var nome2 = 'Ferrarezi';
+// 		let nome = 'Outra coisa'; // redeclarando
+
+// 	}
+// }
+
+// console.log(nome, nome2);
+
+
+// function falaOi() {
+// 	if (verdadeira) {
+// 		var sobrenome = 'Antonio';
+// 		let nome = 'Franccesco';
+// 	}
+// 	console.log(sobrenome);
+// }
+
+// falaOi();
+
+let sobrenome = 'Bordon';
+console.log(sobrenome);
+
