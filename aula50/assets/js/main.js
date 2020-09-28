f3d6965f85353de34edff70bb64e2533f0a88aba
Franccesco Antonio <franccesco_@hotmail.com>
2020-09-28 20:04:12 -0300

    aula50

---
+++ aula50/assets/js/main.js
@@ -0,0 +1,11 @@
+const conta = (...args) => {
+	// for (let numero of numeros) {
+	// 	if(operador === '+') acumulador += numero;
+	// 	if(operador === '-') acumulador -= numero;
+	// 	if(operador === '/') acumulador /= numero;
+	// 	if(operador === '*') acumulador *= numero;
+	// }
+	console.log(args);
+};
+
+conta('+', 1, 20, 30, 40, 50);
