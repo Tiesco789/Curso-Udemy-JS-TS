4b7626adc3ea3a68f66f50cd736790a3635e6c10
Franccesco Antonio <franccesco_@hotmail.com>
2020-09-28 10:03:32 -0300

    aula37

---
+++ aula37/assets/js/main.js
@@ -0,0 +1,30 @@
+const pessoa = {
+	nome: 'Franccesco',
+	sobrenome: 'Antonio',
+}
+
+// For clássico - Geralmente iteráveis (array ou strings);
+// For in - Retorna o indice ou chave (string, array ou objetos);
+// For of - Retorna o valor em si (iteraveis, array ou strings);
+
+// for (const chave in pessoa) {
+// 	if (pessoa.hasOwnProperty(chave)) {
+// 		console.log();
+// 	}
+// }
+
+// for (let i = 0; i < nome.length; i++) {
+// 	console.log(nome[i]);
+// }
+
+// for (let i in nome) {
+// 	console.log(nome[i]);
+// }
+
+// for (let valor of nome) {
+// 	console.log(valor);
+// }
+
+// nome.forEach((valor, indice, array) => {
+// 	console.log(valor, indice, array);
+// });
