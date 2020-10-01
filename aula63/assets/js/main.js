d20fdce1f0b29a11fcd70a656ac9169a49e063f4
Franccesco Antonio <franccesco_@hotmail.com>
2020-09-30 21:40:45 -0300

    aula63

---
+++ aula63/assets/js/main.js
@@ -0,0 +1,11 @@
+const nomes = [ 'Franccesco', 'Jessica', 'Jaqueline', 'Robson' ];
+
+nomes.splice(0, 0, 'Luiz', 'Otavio')
+
+console.log(nomes);
+
+// const removidos = nomes.splice(3, 0, 'Luiz')
+// console.log(nomes, removidos);
+
+// const removidos = nomes.splice(0, 1);
+// console.log(nomes, removidos);
