30db4b2d256d9e5cfcee6c0b3771334b73d60b72
Franccesco Antonio <franccesco_@hotmail.com>
2020-09-30 21:26:37 -0300

    aula62

---
+++ aula62/assets/js/main.js
@@ -0,0 +1,13 @@
+// Valore por referencia
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// Stromg, Objetos, Funções, Numeros
+// const novo = nomes.slice(1, -2);
+// console.log(novo);
+
+// const nome = 'Franccesco Antonio Ferrarezi Bordon';
+// const nomes = nome.split(' ');
+
+const nomes = [ 'Franccesco', 'Antonio', 'Ferrarezi', 'Bordon' ];
+const nome = nomes.join(' ')
+
+console.log(nome);
