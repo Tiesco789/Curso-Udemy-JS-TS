0bd6c79a4137c7fa32aee504e8d1256aba4d861a
Franccesco Antonio <franccesco_@hotmail.com>
2020-10-29 19:03:59 -0300

    aula87

--- aula87/assets/js/main.js
+++ aula87/assets/js/main.js
@@ -7,28 +7,43 @@ function rand(min, max) {
 
 function esperaAi(msg, tempo) {
 	return new Promise((resolve, reject) => {
-		if (typeof msg !== 'string') reject(false);
-			setTimeout(() => {
+		setTimeout(() => {
+			if (typeof msg !== 'string') {
+				reject('Cai no erro');
+				return;
+			}
 				resolve(msg.toUpperCase() + ' - Passei na promise');
+				return;
 		}, tempo);
 	});
 }
 
-// Promise.all Promise.race Promise.resolve Promise.reject
-const promises = [
-  'Primeiro valor',
-  esperaAi('Promise 1', 3000),
-  esperaAi('Promise 2', 500),
-  esperaAi('Promise 3', 1000),
-  esperaAi(1000, 1000),
+// // Promise.all Promise.race Promise.resolve Promise.reject
+// const promises = [
+//   esperaAi('Promise 1', rand(1, 5)),
+//   esperaAi('Promise 2', rand(1, 5)),
+//   esperaAi('Promise 3', rand(1, 5)),
+//   esperaAi(1000, rand(1, 5)),
+// ];
 
-  'Outro valor',
-];
+// Promise.race(promises)
+// 	.then(function (valor) {
+// 		console.log(valor);
+// 	})
+// 	.catch(function (erro) {
+// 		console.log(erro);
+// 	})
 
-Promise.all(promises)
-	.then(function (valor) {
-		console.log(valor);
-	})
-	.catch(function (erro) {
-		console.log(erro);
-	})
+function baixaPagina() {
+	const emCache = true;
+
+	if (emCache) {
+		return Promise.reject('Página em cache');
+	} else {
+		return esperaAi('Baixei a página', 3000)
+	}
+}
+
+baixaPagina().then(dadosPagina => {
+	console.log(dadosPagina);
+}).catch(e => console.log('Erro', e));
