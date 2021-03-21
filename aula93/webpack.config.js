6f5b35f1ff8116feb69884b5a3c32af490ea0771
Franccesco Antonio <franccesco_@hotmail.com>
2021-03-21 12:28:53 -0300

    aula93

---
+++ aula93/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path'); // CommonJS
+
+const isDevelopment = process.env.NODE_ENV !== 'development';
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'js'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+          },
+        },
+      },
+    ],
+  },
+};
